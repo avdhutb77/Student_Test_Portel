7a4e2e48c12fc012be1a53113b1ffcbf3753edf3
avdhutb77 <avdhutbarbole51@gmail.com>
2024-09-30 12:53:38 +0530

    Add function which display Class wise result

--- studenttestportel.js
+++ studenttestportel.js
@@ -4,7 +4,42 @@ import readline from 'readline-sync';
 const student = [{
     rollNo: 101,
     name: "Avdhut",
-    class: "10th Grade",
+    class: 10,
+    gender: "Male",
+    testScores: [
+        { subName: "Math", marks: "" },
+        { subName: "Science", marks: "" },
+        { subName: "English", marks: "" }
+    ],
+
+},
+{
+    rollNo: 102,
+    name: "rahul",
+    class: 10,
+    gender: "Male",
+    testScores: [
+        { subName: "Math", marks: "" },
+        { subName: "Science", marks: "" },
+        { subName: "English", marks: "" }
+    ],
+
+},
+{
+    rollNo: 103,
+    name: "Avi",
+    class: 10,
+    gender: "Male",
+    testScores: [
+        { subName: "Math", marks: "" },
+        { subName: "Science", marks: "" },
+        { subName: "English", marks: "" }
+    ],
+
+},{
+    rollNo: 103,
+    name: "sham",
+    class: 9,
     gender: "Male",
     testScores: [
         { subName: "Math", marks: "" },
@@ -17,13 +52,15 @@ const student = [{
 
 
 ];
+displayMenu();
 
 function displayMenu() {
     console.log(`**** Display Menu ****\n
         1) Take a Test \n
         2) Generate Result \n
         3) View Result \n
-        4) Exit`);
+        4) View class Result \n
+        5) Exit`);
 
     const userIp = readline.question("Enter your option: ");
 
@@ -34,6 +71,8 @@ function displayMenu() {
     } else if (userIp == 3) {
         handleViewResult();
     } else if (userIp == 4) {
+        handleClassResult();
+    }else if (userIp == 5) {
         console.log("Exiting the program. Goodbye!");
         return; 
     } else {
@@ -68,10 +107,17 @@ function handleGenerateResult() {
     
     
     averageScore = totalMarks / st.testScores.length;
+
+    st.Total_Mark = totalMarks.toString();
+    st.Percentage=averageScore.toFixed(2);
+
+    console.log(`Total Marks: of ${st.name} is  ${totalMarks}`);
+    console.log(`Average Score: ${averageScore.toFixed(2)}\n\n`);
+
     }
     
-    console.log(`Total Marks: ${totalMarks}`);
-    console.log(`Average Score: ${averageScore.toFixed(2)}`); // Format to 2 decimal places
+    //console.log(`Total Marks: ${totalMarks}`);
+    //console.log(`Average Score: ${averageScore.toFixed(2)}`); // Format to 2 decimal places
 
     displayMenu(); 
 }
@@ -79,6 +125,8 @@ function handleGenerateResult() {
 
 function handleViewResult() {
     console.log("Viewing result...");
+    let totalMarks;
+    let averageScore;
     for (let st of student){
 
    
@@ -86,7 +134,7 @@ function handleViewResult() {
 +------+--------------------+----------------+--------+
 | Roll |        Name        |      Class     | Gender |
 +------+--------------------+----------------+--------+
-| ${st.rollNo}  |  ${st.name.padEnd(18)}| ${st.class.padEnd(14)} | ${st.gender.padEnd(6)} |
+| ${st.rollNo}  |  ${st.name.padEnd(18)}| ${st.class}             | ${st.gender.padEnd(6)} |
 +------+--------------------+----------------+--------+`);
 
     
@@ -102,13 +150,39 @@ function handleViewResult() {
     console.log(`+--------------------+-------+`);
 
     
+     
     }
 
-    console.log(`Total Marks: ${totalMarks}`);
-    console.log(`Average Score: ${averageScore.toFixed(2)}`);
+    
 
     displayMenu();
 }
 
-// Start the menu
-displayMenu();
+
+
+function handleClassResult(){const a=parseInt(readline.question("Enter class: "));
+    let mark=0;
+    let count=0;
+    let total=0;
+
+
+    for(let st of student){
+        if(st.class==a){
+            count++;
+            total = st.testScores.reduce((sum, score) => {
+                return sum + (parseFloat(score.marks) || 0);
+         },0);
+        }else{
+        continue;
+        }
+        mark=mark+total;
+    
+    }
+    //count=count*3;
+    console.log("Average Total_Marks of student class",a," is= ",mark/count)
+    count=count*300;
+    const averageScore = (mark / count) * 100 ;
+    console.log("Average percentage of class ",a, " is- ",averageScore.toFixed(2));
+
+    displayMenu();
+}
