8cd45bc56ceeea24d1c327c20312bbc9329aeb11
avdhutb77 <avdhutbarbole51@gmail.com>
2024-10-01 15:02:33 +0530

    Add detail view of class result

--- studenttestportel.js
+++ studenttestportel.js
@@ -4,7 +4,7 @@ import readline from 'readline-sync';
 const student = [{
     rollNo: 101,
     name: "Avdhut",
-    class: 10,
+    class: 8,
     gender: "Male",
     testScores: [
         { subName: "Math", marks: "" },
@@ -19,7 +19,7 @@ const student = [{
 {
     rollNo: 102,
     name: "rahul",
-    class: 10,
+    class: 8,
     gender: "Male",
     testScores: [
         { subName: "Math", marks: "" },
@@ -33,7 +33,7 @@ const student = [{
 {
     rollNo: 104,
     name: "Avi",
-    class: 10,
+    class: 8,
     gender: "Male",
     testScores: [
         { subName: "Math", marks: "" },
@@ -56,6 +56,32 @@ const student = [{
     Total_Mark:"",
     Percentage:"",
 
+},{
+    rollNo: 106,
+    name: "sham",
+    class: 9,
+    gender: "Male",
+    testScores: [
+        { subName: "Math", marks: "" },
+        { subName: "Science", marks: "" },
+        { subName: "English", marks: "" }
+    ],
+    Total_Mark:"",
+    Percentage:"",
+
+},{
+    rollNo: 107,
+    name: "sham",
+    class: 9,
+    gender: "Male",
+    testScores: [
+        { subName: "Math", marks: "" },
+        { subName: "Science", marks: "" },
+        { subName: "English", marks: "" }
+    ],
+    Total_Mark:"",
+    Percentage:"",
+
 }
 
 ];
@@ -65,9 +91,10 @@ function displayMenu() {
     console.log(`**** Display Menu ****\n
         1) Take a Test \n
         2) Generate Result \n
-        3) View Result \n
-        4) View class Result \n
-        5) Exit`);
+        3) View all students Result \n
+        4) View Class Result \n
+        5) Detail Analysis of Result\n
+        6) Exit`);
 
     const userIp = readline.question("Enter your option: ");
 
@@ -80,6 +107,8 @@ function displayMenu() {
     } else if (userIp == 4) {
         handleClassResult();
     }else if (userIp == 5) {
+        handleDetailResult();
+    }else if (userIp == 6) {
         console.log("Exiting the program. Goodbye!");
         return; 
     } else {
@@ -185,6 +214,88 @@ function handleClassResult(){
     count=count*300;
     const averageScore = (mark / count) * 100 ;
     console.log("Average percentage of class ",a, " is- ",averageScore.toFixed(2),"%");
+
+    displayMenu();
+}
+
+function handleDetailResult() {
+    const Classdata = {};  
+
+    for (let st of student) {
+        
+        if (!Classdata[st.class]) {
+            Classdata[st.class] = {
+                students: 0,
+                totalMarks: 0,
+                totalPercentage: 0,
+                overallGrade: "",
+                failedStudents: 0,
+                passedStudents: 0
+            };
+        }
+
+        
+        Classdata[st.class].students += 1;
+
+       
+        Classdata[st.class].totalMarks += parseFloat(st.Total_Mark);
+        Classdata[st.class].totalPercentage += parseFloat(st.Percentage);
+
+        
+        if (parseFloat(st.Percentage) <= 35) {
+            Classdata[st.class].failedStudents += 1;
+        } else {
+            Classdata[st.class].passedStudents += 1;
+        }
+    }
+
+   
+    for (let classKey in Classdata) {
+        let classData = Classdata[classKey];
+
+        classData.AvgMarks = (classData.totalMarks / classData.students).toFixed(2);
+        classData.AvgPercentage = (classData.totalPercentage / classData.students).toFixed(2);
+
+        classData.failedStudentsPercentage = ((classData.failedStudents / classData.students) * 100).toFixed(2);
+        classData.passedStudentsPercentage = ((classData.passedStudents / classData.students) * 100).toFixed(2);
+
+        
+        classData.overallGrade = getOverallGrade(classData.AvgPercentage);
+    }
+
+    
+    function getOverallGrade(avgPercentage) {
+        avgPercentage = parseFloat(avgPercentage);
+        if (avgPercentage >= 90) {
+            return "A";
+        } else if (avgPercentage >= 80) {
+            return "B";
+        } else if (avgPercentage >= 70) {
+            return "C";
+        } else if (avgPercentage >= 60) {
+            return "D";
+        } else if (avgPercentage >= 50) {
+            return "E";
+        } else if (avgPercentage >= 40) {
+            return "F";
+        } else {
+            return "FAIL";
+        }
+    }
+
+    
+    console.log(`
+        +---------+------------------+---------+--------------+--------------+---------------+---------+---------------+---------+
+        | Class   |  Total Students  |Avg Marks|Avg Percentage|Overall Grade |Failed Students|Failed % |Passed Students|Passed % |
+        +---------+------------------+---------+--------------+--------------+---------------+---------+---------------+---------+`);
+
+    for (let classKey in Classdata) {
+        let classData = Classdata[classKey];
+        console.log(`        |  ${classKey}      |         ${classData.students}        | ${classData.AvgMarks}  |     ${classData.AvgPercentage}    |      ${classData.overallGrade}       |    ${classData.failedStudents}          |   ${classData.failedStudentsPercentage}  |       ${classData.passedStudents}       |   ${classData.passedStudentsPercentage} |`);
+    }
+
+    console.log(`        +---------+------------------+---------+--------------+--------------+---------------+---------+---------------+---------+
+    `);
 
     displayMenu();
 }
